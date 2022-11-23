9e75c618a25093b4c552690a5e3db31b8d369fa1
Pau Valls <>
2022-11-23 17:37:44 +0100

    commit with base code and first tests

---
+++ src/main/java/GetLongestPrefix.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public final class GetLongestPrefix {
+
+  //  Using Java or C#, please write a working solution:
+  //  to find the longest common prefix string amongst an array of strings. If there is no
+  //  common prefix, return an empty string;.
+
+  //1 <= strs.length <= 200
+  //0 <= strs[i].length <= 20
+
+  public static void main(final String[] args) throws Exception {
+    final List<String> listOfWords = Arrays.asList("flower", "flowerpower", "floght");
+    checkContstrains(listOfWords);
+    final var prefix = getLongestPrefix(listOfWords);
+    System.out.println("the longest prefix is " + prefix);
+  }
+
+  public static void checkContstrains(final List<String> listOfWords) throws Exception {
+    if (listOfWords.stream().anyMatch(s -> s.length() > 20)) {
+      throw new Exception("Constrains executed: the words can't be more than 20 characters");
+    }
+    if (listOfWords.size() > 200) {
+      throw new Exception("Constrains executed: the list can't be more than 200 words");
+    }
+    if (listOfWords.stream().allMatch(s -> s.matches("^[a-z]+$"))) {
+      throw new Exception("Constrains executed: the word");
+    }
+  }
+
+  public static String getLongestPrefix(final List<String> listOfWords) {
+    var prefix = "";
+    int i = 0;
+
+    final var minLenghtWord = listOfWords
+        .stream()
+        .min(Comparator.comparingInt(String::length))
+        .get()
+        .length();
+
+    while (i < minLenghtWord && listOfWords.get(0).charAt(i) == listOfWords.get(listOfWords.size() - 1).charAt(i)) {
+      i++;
+      prefix = listOfWords.get(0).substring(0, i);
+    }
+
+    final String finalPrefix = prefix;
+
+    final var discrepancy = listOfWords
+        .stream()
+        .anyMatch(x -> !x.startsWith(finalPrefix));
+
+    if (discrepancy) {
+      return "";
+    }
+    return prefix;
+  }
+
+}
---
+++ src/test/java/GetLongestPrefixTest.java
@@ -0,0 +1,72 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+
+class GetLongestPrefixTest {
+
+  @Test
+  void should_return_exception_when_input_have_a_word_with_21_characters() {
+    // given
+    final List<String> listOfWords = Arrays.asList("flower", "ihaveMoreThan21characters", "floght");
+    // when
+    final Exception exception = assertThrows(Exception.class, () -> GetLongestPrefix.checkContstrains(listOfWords));
+    // then
+    assertEquals(exception.getMessage(), "Constrains executed: the words can't be more than 20 characters");
+  }
+
+  @Test
+  void should_return_exception_when_input_have_more_than_200_elements() {
+    // given
+    final var intArray = IntStream.rangeClosed(0, 200).toArray();// From 0 to 100
+    final var listOfWords = Arrays.stream(intArray)
+        .mapToObj(String::valueOf)
+        .collect(Collectors.toList());
+    // when
+    final var exception = assertThrows(Exception.class, () -> GetLongestPrefix.checkContstrains(listOfWords));
+    // then
+    assertEquals(exception.getMessage(), "Constrains executed: the list can't be more than 200 words");
+  }
+
+  @Test
+  void should_return_empty_when_list_does_not_have_any_prefix() {
+    // given
+    final var listOfWords = Arrays.asList("flower", "ihaveMoreThan21characters", "floght");
+    // when
+    final var result = GetLongestPrefix.getLongestPrefix(listOfWords);
+    // then
+    assertEquals(result, "");
+  }
+  @Test
+  void should_return_empty_when_list_has_empty_string() {
+    // given
+    final var listOfWords = Arrays.asList("flower", "", "floght");
+    // when
+    final var result = GetLongestPrefix.getLongestPrefix(listOfWords);
+    // then
+    assertEquals(result, "");
+  }
+  @Test
+  void should_return_flo_when_list_has_a_prefix() {
+    // given
+    final var listOfWords = Arrays.asList("flower", "flowerpower", "floght");
+    // when
+    final var result = GetLongestPrefix.getLongestPrefix(listOfWords);
+    // then
+    assertEquals(result, "flo");
+  }
+  @Test
+  void should_return_flower_when_list_has_a_word_like_a_prefix() {
+    // given
+    final var listOfWords = Arrays.asList("flower", "flowerpower", "flowerpowerpower");
+    // when
+    final var result = GetLongestPrefix.getLongestPrefix(listOfWords);
+    // then
+    assertEquals(result, "flower");
+
+  }
+}
